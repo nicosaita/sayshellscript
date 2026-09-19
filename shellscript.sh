086d1e09b0d76988ebeb6640ac5e7367239e3cd4
nicosaita <nico_saita@horacemann.org>
2026-09-18 21:21:45 -0400

    Add files via upload

---
+++ shellscript.sh
@@ -0,0 +1,6 @@
+echo "Input your name:"
+read -r yourname
+echo "Input your favorite flavor of ice cream:"
+read -r icecream
+echo "Welcome $yourname. Your favorite flavor of icecream is $icecream. That's a good choice!"
+say -v "Reed" "Welcome $yourname. Your favorite flavor of icecream is $icecream. That's a good choice!"
